bed9a6676dd14b469f4043e09d55683f8b2d1180
jvaquero <jvaquero@aidacanarias.com>
2022-04-12 07:31:30 +0100

    test case create anagrams for four letters input

--- AnagramasKata.Tests/AnagramsCreatorShould.cs
+++ AnagramasKata.Tests/AnagramsCreatorShould.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System.Runtime.InteropServices;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace AnagramasKata.Tests {
@@ -19,10 +20,19 @@ namespace AnagramasKata.Tests {
             call.Should().Throw<ArgumentNullException>();
         }
 
-        [TestCase("a",   new[] { "a" }, TestName = "create anagrams for single letter input")]
-        [TestCase("ab",  new[] { "ab", "ba" }, TestName = "create anagrams for two letters input")]
-        [TestCase("aa",  new[] { "aa" }, TestName = "create non duplicated anagrams")]
-        [TestCase("abc", new[] {"abc", "acb", "bac", "bca", "cab", "cba"}, TestName = "create anagrams for three letters input")]
+        [TestCase("a",   new[] { "a" }, 
+            TestName = "create anagrams for single letter input")]
+        [TestCase("ab",  new[] { "ab", "ba" }, 
+            TestName = "create anagrams for two letters input")]
+        [TestCase("aa",  new[] { "aa" }, 
+            TestName = "create non duplicated anagrams")]
+        [TestCase("abc", new[] {"abc", "acb", "bac", "bca", "cab", "cba"}, 
+            TestName = "create anagrams for three letters input")]
+        [TestCase("abcd", new[] {"abcd", "abdc", "acbd", "acdb", "adbc", "adcb",
+                                 "bacd", "badc", "bcad", "bcda", "bdac", "bdca",
+                                 "cabd", "cadb", "cbad", "cbda", "cdab", "cdba",
+                                 "dabc", "dacd", "dbac", "dbca", "dcab", "dcba" },
+            TestName = "create anagrams for four letters input")]
         public void return_all_combinations_only_once(string input, string[] expected) {
             var result = anagramsCreator.GetAnagrams(input);
 
