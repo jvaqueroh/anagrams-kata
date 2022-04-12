d8de510d8f952a904f32587d295aacfc5ed70573
jvaquero <jvaquero@aidacanarias.com>
2022-04-12 07:28:03 +0100

    refactor set test cases names

--- AnagramasKata.Tests/AnagramsCreatorShould.cs
+++ AnagramasKata.Tests/AnagramsCreatorShould.cs
@@ -19,10 +19,10 @@ namespace AnagramasKata.Tests {
             call.Should().Throw<ArgumentNullException>();
         }
 
-        [TestCase("a",   new[] { "a" })]
-        [TestCase("ab",  new[] { "ab", "ba" })]
-        [TestCase("aa",  new[] { "aa" })]
-        [TestCase("abc", new[] {"abc", "acb", "bac", "bca", "cab", "cba"})]
+        [TestCase("a",   new[] { "a" }, TestName = "create anagrams for single letter input")]
+        [TestCase("ab",  new[] { "ab", "ba" }, TestName = "create anagrams for two letters input")]
+        [TestCase("aa",  new[] { "aa" }, TestName = "create non duplicated anagrams")]
+        [TestCase("abc", new[] {"abc", "acb", "bac", "bca", "cab", "cba"}, TestName = "create anagrams for three letters input")]
         public void return_all_combinations_only_once(string input, string[] expected) {
             var result = anagramsCreator.GetAnagrams(input);
 
