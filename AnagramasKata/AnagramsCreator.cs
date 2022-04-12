40efd7caaafe3468b48804ff76baf31a257242ca
jvaquero <jvaquero@aidacanarias.com>
2022-04-12 07:14:24 +0100

    feat three characters input anagrams

--- AnagramasKata/AnagramsCreator.cs
+++ AnagramasKata/AnagramsCreator.cs
@@ -4,15 +4,16 @@
             if (string.IsNullOrWhiteSpace(input))
                 throw new ArgumentNullException();
 
-            var result = new List<string>() { input };
+            var result = new List<string>();
 
-            if (input.Length > 1)
-                result.Add(
-                    new string(
-                        input.ToCharArray()
-                        .Reverse()
-                        .ToArray())
-                );
+            for (int i = 0; i < input.Length; i++) {
+                var initial = input[i];
+                var rest = input.ToList();
+                rest.RemoveAt(i);
+                result.Add(initial + new string(rest.ToArray()));
+                rest.Reverse();
+                result.Add(initial + new string(rest.ToArray()));
+            }
 
             return result.Distinct().ToList();
         }
